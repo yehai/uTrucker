dd4a25518086ef1ee8768efb187c038851ab26a9
yehai <yehai2050@hotmail.com>
2013-01-07 16:12:21 +0800

    added unity test. fixed .xcworkspace ignore.

---
+++ uTrucker/test/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  test
+//
+//  Created by qiup on 13-1-7.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface test : SenTestCase
+
+@end
---
+++ uTrucker/test/test.m
@@ -0,0 +1,32 @@
+//
+//  test.m
+//  test
+//
+//  Created by qiup on 13-1-7.
+//
+//
+
+#import "test.h"
+
+@implementation test
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in test");
+}
+
+@end
